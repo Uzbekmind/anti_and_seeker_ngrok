c48473b40fe74da842481d4b0338bcabeb72b213
Uzbekmind <67470704+Uzbekmind@users.noreply.github.com>
2020-06-29 18:43:53 +0500

    Update termux_install.sh

--- termux_install.sh
+++ termux_install.sh
@@ -1,18 +1,18 @@
-echo '[!] Updating...'
+echo '[!] Обновление...'
 apt-get update > install.log
 echo
-echo '[!] Installing Dependencies...'
+echo '[!] Установка требований...'
 echo '    Python3'
 apt-get -y install python &>> install.log
 echo '    PHP'
 apt-get -y install php &>> install.log
 echo '    ssh'
 apt-get -y install openssh &>> install.log
-echo '    Requests'
+echo '    Подключение...'
 pip3 install requests &>> install.log
 echo
-echo '[!] Setting Permissions...'
+echo '[!] Завершение...'
 chmod 777 template/nearyou/php/info.txt
 chmod 777 template/nearyou/php/result.txt
 echo
-echo '[!] Installed.'
+echo '[!] Успешно.'
