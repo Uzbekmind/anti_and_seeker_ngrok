916569289d53c4f93ee43c7840d2611054ccc81c
Uzbekmind <67470704+Uzbekmind@users.noreply.github.com>
2020-06-29 22:02:27 +0500

    Update arch_install.sh

--- arch_install.sh
+++ arch_install.sh
@@ -1,7 +1,7 @@
-echo '[!] Updating...'
+echo '[!] Обновление...'
 pacman -Sy > install.log
 echo
-echo '[!] Installing Dependencies...'
+echo '[!] Установка требований...'
 echo '    Python3'
 yes | pacman -S python3 python-pip --needed &>> install.log
 echo '    PHP'
@@ -11,8 +11,8 @@ yes | pacman -S openssh --needed &>> install.log
 echo '    Requests'
 pip3 install requests &>> install.log
 echo
-echo '[!] Setting Permissions...'
+echo '[!] Настройка разрешений...'
 chmod 777 template/nearyou/php/info.txt
 chmod 777 template/nearyou/php/result.txt
 echo
-echo '[!] Installed.'
+echo '[!] Успешно.'
